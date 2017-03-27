d5586c4713c446efe54148c6a1ed1b0dc52af891
radeknovotny94 <radeknovotny94@gmail.com>
2017-03-27 13:20:17 +0200

    update erosionusle

--- pyerosion/erosionusle.py
+++ pyerosion/erosionusle.py
@@ -17,7 +17,10 @@ class ErosionUSLE(ErosionBase):
                                 'tci' : 'tci',
                                 'hpj' : 'hpj',
                                 'kpp' : 'kpp',
-                                'hpj_kpp' : 'hpj_kpp'}
+                                'hpj_kpp' : 'hpj_kpp',
+                                'landuse' : 'landuse',
+                                'hpj_kpp_land' : 'hpj_kpp_land',
+                                'hpj_kpp_land_rastr' : 'hpj_kpp_land_rastr'}
         
     def run(self):
         run_command('g.region', raster=self._maps['dmt'])
@@ -29,8 +32,20 @@ class ErosionUSLE(ErosionBase):
         run_command('r.mapcalc', expr=formula)
 #        KC Factor
         run_command('v.overlay', ainput=self._maps['hpj'], binput=self._maps['kpp'], operator='or', output=self._maps['hpj_kpp'])
-
-    def test(self):
+        run_command('v.overlay', ainput=self._maps['hpj_kpp'], binput=self._maps['landuse'], operator='and', output=self._maps['hpj_kpp_land'])
+        run_command('v.db.addcolumn', map=self._maps['hpj_kpp_land'], columns='K double')
+        run_command('v.db.addcolumn', map=self._maps['hpj_kpp_land'], columns='C double')
+        run_command('v.db.join', map=self._maps['hpj_kpp_land'], column='a_a_HPJ', other_table='hpj_k1', other_column='HPJ')
+        run_command('db.execute', sql='UPDATE hpj_kpp_land SET K = (SELECT b.K FROM hpj_kpp_land AS a JOIN kpp_k3 as b ON a.a_b_KPP = b.KPP) WHERE K IS NULL')
+        run_command('v.db.join', map=self._maps['hpj_kpp_land'], column='b_LandUse', other_table='lu_c1', other_column='LU')
+        run_command('v.db.addcolumn', map=self._maps['hpj_kpp_land'], columns='KC double')
+        run_command('v.db.update', map=self._maps['hpj_kpp_land'], column='KC', query_column='K * C')
+#        Final G Factor
+        run_command('v.to.rast', input=self._maps['hpj_kpp_land'], output=self._maps['hpj_kpp_land_rastr'], use='attr', attribute_column='KC')
+        formula1='g = 40 * ls*' + self._maps['hpj_kpp_land_rastr'] +' * 1'
+        run_command('r.mapcalc', expr=formula1)
+        
+def test(self):
         run_command('g.region', flags='p')
         run_command('r.info', map=self._maps['slope'])
         time.sleep(10)
