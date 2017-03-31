4aee39e451716fd60d52645c12cee16a4efdead9
radeknovotny94 <radeknovotny94@gmail.com>
2017-03-31 23:55:56 +0200

    add load layer - dmt, euc

--- soil_erosion_dockwidget.py
+++ soil_erosion_dockwidget.py
@@ -23,8 +23,13 @@
 
 import os
 
+from PyQt4.QtCore import QSettings, pyqtSignal, QFileInfo
+from PyQt4.QtGui import QFileDialog
+from qgis.core import QgsProviderRegistry
+from qgis.utils import iface
+from qgis.gui import QgsMapLayerProxyModel
+
 from PyQt4 import QtGui, uic
-from PyQt4.QtCore import pyqtSignal
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'soil_erosion_dockwidget_base.ui'))
@@ -44,7 +49,52 @@ class SoilErosionDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
+        self.settings = QSettings("CTU","Erosion_plugin")
+
+        self.iface = iface
+
+        # Set filters for QgsMapLayerComboBoxes
+        self.raster_box.setFilters(QgsMapLayerProxyModel.RasterLayer)
+        self.load_raster.clicked.connect(self.onLoadRaster)
+
+        self.shp_box.setFilters(QgsMapLayerProxyModel.PolygonLayer)
+        self.load_shp.clicked.connect(self.onLoadShapefile)
+        
+
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
 
+    def onLoadRaster(self):
+        """Open 'Add raster layer dialog'."""
+        sender = '{}-lastUserFilePath'.format(self.sender().objectName())
+        lastUsedFilePath = self.settings.value(sender, '')
+
+        fileName = QFileDialog.getOpenFileName(self,self.tr(u'Open Raster'), 
+                                               self.tr(u'{}').format(lastUsedFilePath),
+                                               QgsProviderRegistry.instance().fileRasterFilters())
+        if fileName:
+            self.iface.addRasterLayer(fileName, QFileInfo(fileName).baseName())
+            self.settings.setValue(sender, os.path.dirname(fileName))
+
+    def onLoadShapefile(self):
+        """Open 'Add shapefile layer dialog'."""
+        sender = '{}-lastUserFilePath'.format(self.sender().objectName())
+        lastUsedFilePath = self.settings.value(sender, '')
+        
+        fileName = QFileDialog.getOpenFileName(self,self.tr(u'Open Shapefile'),
+                                               self.tr(u'{}').format(lastUsedFilePath), 
+                                               QgsProviderRegistry.instance().fileVectorFilters())
+        if fileName:
+            self.iface.addVectorLayer(fileName, QFileInfo(fileName).baseName(), "ogr")
+            self.settings.setValue(sender, os.path.dirname(fileName))
+
+    def AddKCFactors(self):
+        """Add K and C factor to attribute table of EUC layer"""
+        
+
+
+
+
+
+        
